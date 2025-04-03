ef2806051edaba602e38c8fb24bb6a4cd778d608
luigiciuf <luigi.ciuffreda@students.uniroma2.eu>
2025-04-03 11:51:26 +0200

    aggiunto file costanti

---
+++ libs/costants.h
@@ -0,0 +1,9 @@
+#ifndef COSTANTS_H
+#define COSTANTS_H
+
+// Definizione di HackSize
+#define HackSize 32  // Dimensione del blocco
+
+
+// TO DO aggiungere costante iterazioni per matrice
+#endif //COSTANTS_H
